bc85a958a6709bc83b323d818eac75fe1a3c5c82
Shkuratov Artem <shuratov2001@mail.ru>
2022-12-05 17:58:47 +0300

    added husky/lint-staged and gcp yaml

--- src/db/appDataSource.ts
+++ src/db/appDataSource.ts
@@ -1,4 +1,5 @@
 import { DataSource } from 'typeorm';
+import 'reflect-metadata';
 
 import dotenv from 'dotenv';
 
@@ -10,7 +11,9 @@ export const myDataSource = new DataSource({
   port: +process.env.MYSQL_PORT || 3307,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.MYSQL_DB || 'yourdb',
+  database: process.env.MYSQL_DATABASE || 'yourdb',
+  connectTimeout: 2000000,
+  acquireTimeout: 2000000,
   entities: [__dirname + '/../**/*.entity.js'],
   migrationsTableName: '__migrations',
   migrations: ['dist/src/**/migration/*.js'],
